49a2c68d3da849727fec3896846a8bc1b296b0c9
cs83312 <a410121060@gmail.com>
2018-08-24 03:12:30 +0800

    Add files via upload

--- LibraryConvert/src/core/DataConvert1xx.java
+++ LibraryConvert/src/core/DataConvert1xx.java
@@ -2,4 +2,16 @@ package core;
 
 public class DataConvert1xx {
 
+	private String setSignal100(String n){
+		String mainAuthorName = "|a"+n.split("%f")[1].split("%")[0];
+		String froiegnName = null;
+		String book = null;
+		
+		
+		String output = "100 1  "+mainAuthorName;
+		
+		return output;
+	}
+	
+	
 }
---
+++ LibraryConvert/src/core/DataConvert2xx.java
@@ -0,0 +1,21 @@
+package core;
+
+public class DataConvert2xx {
+
+	
+	private String setSignal240(String n){
+		
+		String prefix = "240 10 ";
+		String translateWorksName =null
+		String language = "|l¤¤¤å";
+		
+		
+		String output =prefix+translateWorksName+language;
+		
+		return output;
+	}
+	
+	
+	
+	
+}
